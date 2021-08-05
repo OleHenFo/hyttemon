754dbf37028a7a211d84364c8777b5a977355156
Ole-Henrik Fossland <olehenfo@stud.ntnu.no>
2021-08-05 20:19:22 +0200

    adding font and bg, simple init code

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import os
+import time,random
+from PIL import Image,ImageDraw,ImageFont
+
+fontTemp = ImageFont.truetype("font/Roboto-Medium.ttf", 136)
+textColor = (200,200,200,150)
+
+tempUte = 14
+tempInne = 23
+
+while(True):
+  im = Image.open("img/bg.png")
+  draw = ImageDraw.Draw(im)
+
+  tempUte = 14 + random.randint(0,1)-1
+  tempInne = 23 + random.randint(0,1)-1
+  
+  draw.text((50, 100),str(tempUte)+"°",textColor,font=fontTemp)
+  draw.text((560, 290),str(tempInne)+"°",textColor,font=fontTemp)
+  im.show()
+  time.sleep(2)
